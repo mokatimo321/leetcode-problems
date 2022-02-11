68ad5631682bee5a561e39303a7edf8fafea4c65
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-02-11 16:38:17 +0530

    Time: 305 ms (31.07%), Space: 102.8 MB (5.18%) - LeetHub

---
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, pair<int, int>> mp;
+        mp[0] = {1, 0};
+        //first -> frequency, second -> index
+        int sm = 0, rem = 0, cnt = 0;
+        for(int i = 0;i<nums.size();i++) {
+            sm += nums[i];
+            rem = sm%k;
+            if(mp.find(rem) != mp.end()) {
+                cnt += mp[rem].first;
+                if(i - mp[rem].second == 0) {
+                    cnt--;
+                }
+                mp[rem].first++;
+                mp[rem].second = i+1;
+            }
+            else {
+                mp[rem].first = 1;
+                mp[rem].second = i+1;
+            }
+        }
+        return cnt;
+    }
+};
