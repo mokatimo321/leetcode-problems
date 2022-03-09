e715e50672b79b1607d3f6a71976b715729671ce
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-03-09 18:15:09 +0530

    Time: 38 ms (68.64%), Space: 27.4 MB (46.37%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         
-        vector<int> left(nums.size(), 0);
-        vector<int> right(nums.size(), 0);
+        vector<int> memo(nums.size(), 0);
+        //vector<int> right(nums.size(), 0);
         
         int mn = INT_MAX, mx = INT_MIN;
         
-        for(int i = 0;i<nums.size();i++) {
-            mx = max(mx, nums[i]);
-            left[i] = mx;
-        }
+        // for(int i = 0;i<nums.size();i++) {
+        //     mx = max(mx, nums[i]);
+        //     left[i] = mx;
+        // }
         
         for(int i = nums.size() - 1;i>=0;i--) {
             mn = min(mn, nums[i]);
-            right[i] = mn;
+            memo[i] = mn;
         }
         
         int first_index = 0, last_index = 0;
         int flag = 0;
         
         for(int i = 0;i<nums.size();i++) {
-            if(nums[i] < left[i] || nums[i] > right[i]) {
+            mx = max(mx, nums[i]);
+            if(nums[i] < mx || nums[i] > memo[i]) {
                 if(flag == 0) {
                     first_index = i;
                     flag = 1;
