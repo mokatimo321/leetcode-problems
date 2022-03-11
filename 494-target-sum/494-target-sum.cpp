53591bca343287efb09e093511992f4124b69b1f
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-03-11 18:55:07 +0530

    Time: 29 ms (52.98%), Space: 12.3 MB (40.92%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,48 +1,34 @@
 class Solution {
 public:
     
-    int sm(vector<int> nums) {
-        int sum = 0;
-        for(int i = 0;i<nums.size();i++) {
-            sum += nums[i];
-        }
-        return sum;
-    }
-    
     int findTargetSumWays(vector<int>& nums, int target) {
         
-        int sum = sm(nums);
-        int req = (sum + target)/2;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int req = (target + sum)/2;
         
-        if(abs(target) > sum || (target + sum) %2 != 0) {
+        if(abs(target) > sum || (sum + target) %2 != 0) {
             return 0;
         }
         
-        vector<vector<int>> dp(nums.size() + 1, vector<int> (req+1, 0));
-        
-        dp[0][0] = 1;
-        
-        for(int i = 1;i<=req;i++) {
-            dp[0][i] = 0;
-        }
-        
-        for(int i = 1;i<dp.size();i++) {
-            for(int j = 0;j<dp[0].size();j++) {
-                if(nums[i-1] > j) {
-                    dp[i][j] = dp[i-1][j];
-                }
-                else {
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
-                }
-            }
-        }
-        
+        vector<vector<int>> dp(nums.size() + 1, vector<int> (req + 1, 0));
         
         for(int i = 0;i<dp.size();i++) {
             for(int j = 0;j<dp[0].size();j++) {
-                cout<<dp[i][j]<<" ";
+                if(i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                }
+                else if(i == 0) {
+                    dp[i][j] = 0;
+                }
+                else {
+                    if(nums[i-1] > j) {
+                        dp[i][j] = dp[i-1][j];
+                    }
+                    else {
+                        dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
+                    }
+                }
             }
-            cout<<endl;
         }
         
         return dp[dp.size() - 1][dp[0].size() - 1];
