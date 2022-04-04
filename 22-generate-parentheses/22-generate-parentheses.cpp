2282ff58182abdc162a9e3c6fd6d0b89c1d2441a
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-04-04 22:27:14 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (46.50%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    vector<string> result;
+    //vector<string> result;
     
-    void generate(string s,int index, int n, int fl, int cnt) {
+    void generate(vector<string> &result,string s,int index, int n, int fl, int cnt) {
         
         if(index == 2*n) {
             result.push_back(s);
@@ -11,19 +11,20 @@ public:
         
         //putting an opening bracket
         if(cnt < n) {
-            generate(s + "(", index + 1, n, fl + 1, cnt + 1);
+            generate(result, s + "(", index + 1, n, fl + 1, cnt + 1);
         }
         
         //putting a closing bracket
         if(fl > 0) {
-            generate(s + ")", index + 1, n, fl - 1, cnt);
+            generate(result, s + ")", index + 1, n, fl - 1, cnt);
         }
         
     } 
     
     
     vector<string> generateParenthesis(int n) {
-        generate("", 0, n, 0, 0);
+        vector<string> result;
+        generate(result, "", 0, n, 0, 0);
         return result;
     }
 };
