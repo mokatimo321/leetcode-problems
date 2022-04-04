bce6ed21ad6f6c5191dff1ab96277e5e6c5a05d1
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-04-04 18:58:48 +0530

    Time: 4 ms (52.85%), Space: 7.6 MB (36.66%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     
-    void mo(vector<int> nums, vector<vector<int>> &ans, vector<int> &subset, int index) {
-        
-        ans.push_back(subset);
-        
+    void generate(vector<vector<int>> &result, vector<int> nums, vector<int> subsets, int index) {
+        result.push_back(subsets);
         for(int i = index;i<nums.size();i++) {
-            
-            subset.push_back(nums[i]);
-            mo(nums, ans, subset, i+1);
-            
-            subset.pop_back();
-            
+            subsets.push_back(nums[i]);
+            generate(result, nums, subsets, i + 1);
+            subsets.pop_back();
         }
-        
         return;
-        
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> subset;
-        vector<vector<int>> ans;
-        mo(nums, ans, subset, 0);
-        return ans;
+        vector<vector<int>> result;
+        generate(result, nums, {}, 0);
+        return result;
     }
 };
