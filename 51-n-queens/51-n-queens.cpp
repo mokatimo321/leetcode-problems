a7807434b8875233d235bcec5e37cb265fb1aa76
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-04-06 21:06:48 +0530

    Time: 72 ms (15.31%), Space: 50.5 MB (5.71%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    
+    
+    bool canPlace(vector<string> board, int i, int j, int n) {
+        
+        int x, y;
+        //to check for left diag
+        x = i, y = j;
+        while(x >= 0 && y >= 0) {
+            if(board[x][y] == 'Q') {
+                return false;
+            }
+            x--, y--;
+        }
+        
+        //to check for right diag
+        x = i, y = j;
+        while(x >= 0 && y <= n - 1) {
+            if(board[x][y] == 'Q') {
+                return false;
+            }
+            x--, y++;
+        }
+        
+        //to check for col
+        for(x = 0; x < i; x++) {
+            if(board[x][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+    
+    void solve(vector<vector<string>> &result, vector<string> board, int sz, int i) {
+        
+        //base case
+        if(i == sz) {
+            result.push_back(board);
+            return;
+        }
+        
+        
+        //recur case
+        for(int j = 0;j<sz;j++) {
+            if(canPlace(board, i, j, sz)) {
+                board[i][j] = 'Q';
+                solve(result, board, sz, i + 1);
+                board[i][j] = '.';
+            }
+        }
+        
+        // return false;
+        
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> result;
+        vector<string> board;
+        string s = "";
+        for(int i = 0;i<n;i++) {
+            s += ".";
+        }
+        for(int i = 0;i<n;i++) {
+            board.push_back(s);
+        }
+        solve(result, board, n, 0);
+        return result;
+    }
+};
