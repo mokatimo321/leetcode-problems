f40789559b6986951d10853d72db4fe99ee12a4c
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-04-07 16:22:28 +0530

    Time: 756 ms (8.12%), Space: 164.2 MB (12.70%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    
+    void solve(int n, vector<int> sub, vector<vector<int>> &result, int temp, int index, int k) {
+        
+        //base case
+        if(sub.size() == k) {
+            result.push_back(sub);
+            return;
+        }
+        
+        
+        //recur case
+        for(int i = 1; i <= n ;i++) {
+            if(i > temp) {
+                sub.push_back(i);
+                solve(n, sub, result, i, index + 1, k);
+                sub.pop_back();
+            }
+        }
+        
+    }
+    
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        //vector<int> sub(k, 0);
+        solve(n, {}, result, 0, 0, k);
+        return result;
+    }
+};
