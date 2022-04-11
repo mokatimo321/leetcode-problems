d7b599ad5312dc87addbde8a43e2706fb28d7362
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-04-11 15:15:02 +0530

    Time: 4 ms (66.56%), Space: 8 MB (28.90%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     
-    void solve(vector<int> sub, vector<vector<int>> &result, vector<int> memo, int index, int sz, vector<int> nums) {
+    void solve(vector<int> nums, vector<vector<int>> &result, int index) {
         
         //base case
-        if(index == sz) {
-            result.push_back(sub);
-            return;
+        if(index == nums.size()) {
+            result.push_back(nums);
         }
         
         
         //recur case
-        for(int i = 0;i<nums.size();i++) {
-            if(memo[i] == 0) {
-                sub[index] = nums[i];
-                memo[i] = 1;
-                solve(sub, result, memo, index + 1, sz, nums);
-                memo[i] = 0;
-            }
-            
+        for(int i = index;i<nums.size();i++) {
+            swap(nums[index], nums[i]);
+            solve(nums, result, index + 1);
+            swap(nums[index], nums[i]);
         }
         
     }
     
+    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-        vector<int> sub(nums.size(), 0), memo(nums.size(), 0);
-        int sz = nums.size();
-        solve(sub, result, memo, 0, sz, nums);
+        solve(nums, result, 0);
         return result;
     }
 };
