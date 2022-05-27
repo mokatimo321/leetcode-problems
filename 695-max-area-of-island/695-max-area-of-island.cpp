c99ded24454faabb9d6efd868743faa2076c274b
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-05-28 00:30:08 +0530

    Time: 45 ms (14.20%), Space: 23.4 MB (49.64%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     
-    void solve(vector<vector<int>> grid, vector<vector<bool>> &visited, int i, int j, int &cnt) {
+    void solve(vector<vector<int>> &grid, int i, int j, int &cnt) {
         
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0 || visited[i][j] == true) {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0 || grid[i][j] == -1) {
             return;
         }
             
         cnt++;
-        visited[i][j] = true;
+        grid[i][j] = -1;
         
-        solve(grid, visited, i-1, j, cnt);
-        solve(grid, visited, i, j+1, cnt);
-        solve(grid, visited, i+1, j, cnt);
-        solve(grid, visited, i, j-1, cnt);
+        solve(grid, i-1, j, cnt);
+        solve(grid, i, j+1, cnt);
+        solve(grid, i+1, j, cnt);
+        solve(grid, i, j-1, cnt);
         
     }
     
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
         int res = 0, cnt = 0;
-        
         
         for(int i = 0;i<grid.size();i++) {
             for(int j = 0;j<grid[0].size();j++) {
-                if(grid[i][j] == 1 && visited[i][j] == false) {
+                if(grid[i][j] == 1) {
                     cnt = 0;
-                    solve(grid, visited, i, j, cnt);
+                    solve(grid, i, j, cnt);
                     res = max(res, cnt);
                 }
             }
