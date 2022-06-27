ddcb3a84becc21fbee54bef02b263ad4801a979b
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-06-27 11:08:01 +0530

    Time: 70 ms (37.15%), Space: 23.6 MB (12.72%) - LeetHub

---
+++ 817-linked-list-components/817-linked-list-components.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        
+        //we will use map as a hashmap
+        // vector<int> map(nums.size()+1, 0);
+        // for(int i = 0;i<nums.size();i++) {
+        //     map[nums[i]] += 1;
+        // }
+        unordered_map<int, int> map;
+        for(auto x : nums) {
+            map[x]++;
+        }
+        
+        //p to traverse the LL
+        ListNode* p = head;
+        int cnt = 0, sz = nums.size();
+        
+        //Let's Traverse 
+        while(p) {
+            
+            if(map[p->val] > 0) {
+                //cout<<p->val<<endl;
+                cnt++;
+            }
+            else {
+                if(cnt > 0) {
+                    cnt--;
+                    sz -= cnt;
+                }
+                cnt = 0;
+            }
+            
+            p = p->next;
+            
+        }
+        
+        if(cnt > 0) {
+            cnt--;
+            sz -= cnt;
+            cnt = 0;
+        }
+        
+        return sz;
+        
+    }
+};
