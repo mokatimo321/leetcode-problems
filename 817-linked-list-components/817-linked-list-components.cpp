d69e8a496f3e869c995f804eca1a972c53b10bf6
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-06-27 11:09:27 +0530

    Time: 70 ms (37.15%), Space: 19.9 MB (82.12%) - LeetHub

--- 817-linked-list-components/817-linked-list-components.cpp
+++ 817-linked-list-components/817-linked-list-components.cpp
@@ -10,17 +10,29 @@
  */
 class Solution {
 public:
+    
+    int len_LL(ListNode* head) {
+        ListNode*p = head;
+        int cnt = 0;
+        while(p) {
+            cnt++;
+            p = p->next;
+        }
+        return cnt;
+    }
+    
+    
     int numComponents(ListNode* head, vector<int>& nums) {
         
         //we will use map as a hashmap
-        // vector<int> map(nums.size()+1, 0);
-        // for(int i = 0;i<nums.size();i++) {
-        //     map[nums[i]] += 1;
-        // }
-        unordered_map<int, int> map;
-        for(auto x : nums) {
-            map[x]++;
+        vector<int> map(len_LL(head), 0);
+        for(int i = 0;i<nums.size();i++) {
+            map[nums[i]] += 1;
         }
+        // unordered_map<int, int> map;
+        // for(auto x : nums) {
+        //     map[x]++;
+        // }
         
         //p to traverse the LL
         ListNode* p = head;
