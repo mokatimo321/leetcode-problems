b04374b5f0827c3f615331d135427b699d938466
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-07-02 16:13:40 +0530

    Time: 174 ms (40.13%), Space: 67.6 MB (90.50%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,8 +4,10 @@ public:
         int best_sum = INT_MIN, current_sum = 0;
         for(int i = 0;i<nums.size();i++) {
             current_sum += nums[i];
-            current_sum = max(current_sum, nums[i]);
             best_sum = max(best_sum, current_sum);
+            if(current_sum < 0) {
+                current_sum = 0;
+            }
         }
         return best_sum;
     }
