c68eb128f26f3b94fa245028423f229d58e02fbf
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-07-02 16:08:23 +0530

    Time: 116 ms (41.13%), Space: 48.5 MB (42.86%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int max_reach = nums.size() - 1;
+        int max_index = nums.size() - 1;
         for(int i = nums.size() - 1;i>=0;i--) {
-            if(i + nums[i] >= max_reach) {
-                max_reach = i;
+            if(i + nums[i] >= max_index) {
+                max_index = i;
             }
         }
-        return max_reach == 0 ? true : false;
+        return max_index == 0 ? true : false;
     }
 };
