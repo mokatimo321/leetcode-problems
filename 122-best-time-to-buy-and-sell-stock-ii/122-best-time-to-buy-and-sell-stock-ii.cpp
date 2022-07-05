a21b408b67da26781b9ab0780321df516cfc4fd9
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-07-05 13:53:25 +0530

    Time: 4 ms (90.87%), Space: 13 MB (54.26%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int maxProfit(vector<int>& nums) {
         
-        //2 pass solution
+        //single pass solution
+        int buy_price = nums[0], sell_price = nums[0];
         int max_prof = 0;
         
-        //cummulative sell
+        
         for(int i = 1;i<nums.size();i++) {
-            if(nums[i] > nums[i-1]) {
-                max_prof += nums[i];
+            if(nums[i] > sell_price) {
+                sell_price = nums[i];
+            }
+            else {
+                max_prof += sell_price - buy_price;
+                buy_price = nums[i];
+                sell_price = nums[i];
             }
         }
         
-        //cummulative buy
-        for(int i = 0;i<nums.size() - 1;i++) {
-            if(nums[i] < nums[i+1]) {
-                max_prof -= nums[i];
-            }
-        }
-        
-        return max_prof;
+        return max_prof + (sell_price - buy_price);
         
     }
 };
