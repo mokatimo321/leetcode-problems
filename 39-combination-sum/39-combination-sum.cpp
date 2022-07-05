9931ec4209f8364d6ffeb764c55b6ee78b86478a
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-07-05 16:41:43 +0530

    Time: 151 ms (19.01%), Space: 49.2 MB (20.18%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-
-    void Sum(vector<int>& candidates, int target, vector<vector<int> >& res, vector<int>& r, int i)
-    {
+    
+    void combine(vector<int> nums, int index, int current_sum, vector<vector<int>> &result, vector<int> fill) {
         
-        if(target == 0)
-        {
-            // if we get exact answer
-            res.push_back(r);
+        //base case
+        if(current_sum <= 0 || index == nums.size()) {
+            if(fill.size() > 0 && current_sum == 0) {
+                result.push_back(fill);
+            }
             return;
         }
         
-        while(i <  candidates.size() && target - candidates[i] >= 0)
-        {
-            // Till every element in the array starting
-            // from i which can contribute to the target
-            r.push_back(candidates[i]);// add them to vector
-            
-            // recur for next numbers
-            Sum(candidates,target - candidates[i],res,r,i);
-            ++i;
-            
-            // Remove number from vector (backtracking)
-            r.pop_back();
+        
+        //recur case
+        for(int i = index;i<nums.size();i++) {
+            fill.push_back(nums[i]);
+            combine(nums, index, current_sum - nums[i], result, fill);
+            index++;
+            fill.pop_back();
         }
-}
+        
+    }
     
-     
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end()); // sort candidates array
-        
-        // remove duplicates
-        candidates.erase(unique(candidates.begin(),candidates.end()),candidates.end());
-        
-        vector<int> r;
-        vector<vector<int> > res;
-        
-        Sum(candidates,target,res,r,0);
-        
-        return res;
+    
+    
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        combine(nums, 0, target, result, {});
+        return result;
     }
 };
