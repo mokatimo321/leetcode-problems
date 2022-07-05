48e87489fc5ea09fbd7d757d1553eb02226aa403
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-07-05 18:32:17 +0530

    Time: 85 ms (7.19%), Space: 57.4 MB (5.00%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void solve(vector<int>& candidates, int target,int index,  vector<int> curr){
+    
+    void combine(vector<int> nums, int index, int curr_sum, vector<vector<int>> &result, vector<int> fill) {
         
-        if(target==0){
-            ans.push_back(curr);
-            return ;
+        //base case
+        if(curr_sum <= 0 || index == nums.size()) {
+            if(curr_sum == 0) {
+                result.push_back(fill);
+            }
+            return;
         }
         
-        for(int k=index; k<candidates.size() ;k++){
-            
-            // becuase of this step we will not include duplicates 
-            if(k>index && candidates[k]==candidates[k-1])
+        
+        //recur case
+        for(int i = index;i<nums.size();i++) {
+            if(i > index && nums[i] == nums[i-1]) {
                 continue;
-            
-            // if the current element is greater , then all the elements after it will be useless , as we had sorted the  array  candidates.
-            if(candidates[k]>target)
-                break;
-            
-            // now if the first two conditions are not satisfied then we can include this in our curr array  and then see recursively that can we get  to the sum target by including other values in the candidates array
-            curr.push_back(candidates[k]);
-            
-            solve(candidates,target-candidates[k],k+1,curr);
-            
-            // backtrack
-            curr.pop_back();
+            }
+            fill.push_back(nums[i]);
+            combine(nums, i+1, curr_sum - nums[i], result, fill);
+            fill.pop_back();
         }
-    }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target){
-        sort(candidates.begin(),candidates.end());
-        vector<int> curr;
-        int index=0;
-        solve(candidates,target,index,curr);
         
-        return ans;
+        
+    }
+    
+    
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        combine(nums, 0, target, result, {});
+        return result;
     }
 };
