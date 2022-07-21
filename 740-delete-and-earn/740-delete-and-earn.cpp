c86dffa3623b5238affb0f33fb9635e62a8e4cd5
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-07-21 17:18:02 +0530

    Time: 33 ms (28.61%), Space: 16.5 MB (15.29%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,47 +1,23 @@
 class Solution {
 public:
-    
-    int mo(vector<vector<int>> nums) {
-        vector<int> dp(nums.size(), 0);
-        for(int i = 0;i<nums.size();i++) {
-            if(i == 0) {
-                dp[i] = nums[i][1];
-            }
-            else if(i == 1) {
-                if(nums[i][0] - nums[i-1][0] == 1) {
-                    dp[i] = max(nums[i][1], nums[i-1][1]);
-                }
-                else {
-                    dp[i] = dp[i-1] + nums[i][1];
-                }
-            }
-            else if(nums[i][0] - 1 == nums[i-1][0]) {
-                if(nums[i][1] + dp[i-2] > dp[i-1]) {
-                    dp[i] = nums[i][1] + dp[i-2];
-                }
-                else {
-                    dp[i] = dp[i-1];
-                }
-            }
-            else {
-                dp[i] = dp[i-1] + nums[i][1];
-            }
-        }
-        return dp[nums.size() - 1];
-    }
-    
-    
     int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i = 0;i<nums.size();i++) {
-            mp[nums[i]]++;
-        }
-        vector<vector<int>> momo;
-        for(auto x : mp) {
-            momo.push_back({x.first, x.first*x.second});
-        }
-        sort(momo.begin(), momo.end());
-        int res = mo(momo);
-        return res;
+    int n = 10001;
+    
+	//take the total sum by each number
+    vector<int> sum(n, 0);
+    vector<int> dp(n, 0);
+    
+    for(auto num: nums){
+        sum[num] += num;
     }
+    
+    dp[0] = 0;
+    dp[1] = sum[1];
+    //now apply the house robbing concept
+    for(int i=2; i<n; i++){
+        dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
+    }
+    
+    return dp[n-1];
+}
 };
