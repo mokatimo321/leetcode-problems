d6686ab1cc57c5340b6050ea087415183fcd572e
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-08-03 11:11:47 +0530

    Time: 13 ms (85.53%), Space: 20.9 MB (81.30%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -13,25 +13,27 @@ class Solution {
 public:
     
     int check(TreeNode* root) {
+        
         if(!root) {
             return 0;
         }
         
         int left = check(root->left);
-        int right = check(root->right);
+        if(left == -1) {
+            return -1;
+        }
         
-        return max(left, right) + 1;
+        int right = check(root->right);
+        if(right == -1) {
+            return -1;
+        }
+        
+        return abs(left - right) > 1 ? -1 : max(left, right) + 1;
+        
     }
     
     
     bool isBalanced(TreeNode* root) {
-        if(!root) {
-            return true;
-        }
-        
-        int left = check(root->left);
-        int right = check(root->right);
-        
-        return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+        return check(root) != -1;
     }
 };
